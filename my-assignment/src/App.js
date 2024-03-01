6e587634ad7d317f2b7e8f0d6a32689a878c52a2
fmagana14 <142852197+fmagana14@users.noreply.github.com>
2024-02-29 23:33:47 -0800

    completed all of the css for file.

--- my-assignment/src/App.js
+++ my-assignment/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div>
+      <label>Bill Total</label>
+      <input type="text" placeholder={"0.00"} />
+      <label>Tip</label>
+      <input type="text" placeholder={"0.00"} />
+      <div className = "split">
+        <label>split</label>
+        <div className = "split-control">
+          <button></button>
+          <button></button>
+
+        </div>
+      </div>
+    </div>  
   );
 }
 
